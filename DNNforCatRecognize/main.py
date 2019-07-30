7b4143410a12bee9db16870c1ce51a11977bdad9
weekdawn <571426779@qq.com>
2019-07-30 16:18:13 +0800

    5层神经网络-分辨猫咪

---
+++ DNNforCatRecognize/main.py
@@ -0,0 +1,105 @@
+# coding=utf-8
+from dnn_app_utils_v2 import *
+
+plt.rcParams['figure.figsize'] = (5.0, 4.0)  # set default size of plots
+plt.rcParams['image.interpolation'] = 'nearest'
+plt.rcParams['image.cmap'] = 'gray'
+
+train_x_orig, train_y, test_x_orig, test_y, classes = load_data()
+
+# 这段可以查看训练数据的图片
+# index = 7
+# plt.imshow(train_x_orig[index])
+# plt.show()
+# print ("y = " + str(train_y[0,index]) + ". It's a " + classes[train_y[0,index]].decode("utf-8") +  " picture.")
+
+# 展示数据集大小
+m_train = train_x_orig.shape[0]
+num_px = train_x_orig.shape[1]
+m_test = test_x_orig.shape[0]
+
+print("Number of training examples: " + str(m_train))
+print("Number of testing examples: " + str(m_test))
+print("Each image is of size: (" + str(num_px) + ", " + str(num_px) + ", 3)")
+print("train_x_orig shape: " + str(train_x_orig.shape))
+print("train_y shape: " + str(train_y.shape))
+print("test_x_orig shape: " + str(test_x_orig.shape))
+print("test_y shape: " + str(test_y.shape))
+
+# Reshape the training and test examples
+train_x_flatten = train_x_orig.reshape(train_x_orig.shape[0],
+                                       -1).T  # The "-1" makes reshape flatten the remaining dimensions
+test_x_flatten = test_x_orig.reshape(test_x_orig.shape[0], -1).T
+
+# Standardize data to have feature values between 0 and 1.
+train_x = train_x_flatten / 255.
+test_x = test_x_flatten / 255.
+
+print("train_x's shape: " + str(train_x.shape))
+print("test_x's shape: " + str(test_x.shape))
+
+### CONSTANTS ###
+layers_dims = [12288, 20, 7, 5, 1]  # 5-layer model
+
+
+# 建立5层深度神经网络模型
+def L_layer_model(X, Y, layers_dims, learning_rate=0.0075, num_iterations=3000, print_cost=False):  # lr was 0.009
+    """
+    Implements a L-layer neural network: [LINEAR->RELU]*(L-1)->LINEAR->SIGMOID.
+
+    Arguments:
+    X -- data, numpy array of shape (number of examples, num_px * num_px * 3)
+    Y -- true "label" vector (containing 0 if cat, 1 if non-cat), of shape (1, number of examples)
+    layers_dims -- list containing the input size and each layer size, of length (number of layers + 1).
+    learning_rate -- learning rate of the gradient descent update rule
+    num_iterations -- number of iterations of the optimization loop
+    print_cost -- if True, it prints the cost every 100 steps
+
+    Returns:
+    parameters -- parameters learnt by the model. They can then be used to predict.
+    """
+    np.random.seed(1)
+    costs = []  # keep track of cost
+
+    # Parameters initialization.
+    parameters = initialize_parameters_deep(layers_dims)
+
+    # Loop (gradient descent)
+    for i in range(0, num_iterations):
+        # Forward propagation: [LINEAR -> RELU]*(L-1) -> LINEAR -> SIGMOID.
+        AL, caches = L_model_forward(X, parameters)
+
+        # Compute cost.
+        cost = compute_cost(AL, Y)
+
+        # Backward propagation.
+        grads = L_model_backward(AL, Y, caches)
+
+        # Update parameters.
+        parameters = update_parameters(parameters, grads, learning_rate)
+
+        # Print the cost every 100 training example
+        if print_cost and i % 100 == 0:
+            print("Cost after iteration %i: %f" % (i, cost))
+        if print_cost and i % 100 == 0:
+            costs.append(cost)
+
+    # plot the cost
+    plt.plot(np.squeeze(costs))
+    plt.ylabel('cost')
+    plt.xlabel('iterations (per tens)')
+    plt.title("Learning rate =" + str(learning_rate))
+    plt.show()
+
+    return parameters
+
+
+# 获取训练好的模型参数
+parameters = L_layer_model(train_x, train_y, layers_dims, num_iterations=2500, print_cost=True)
+
+# 训练精确度
+pred_train = predict(train_x, train_y, parameters)
+
+# 测试精确度
+pred_test = predict(test_x, test_y, parameters)
+
